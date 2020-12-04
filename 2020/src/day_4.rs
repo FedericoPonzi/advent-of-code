783b78196d68ad7b44018e20389e8d5e38948fc5
Federico Ponzi <isaacisback92@gmail.com>
2020-12-04 19:06:02 +0000

    Day 4 :tada:

---
+++ 2020/src/day_4.rs
@@ -0,0 +1,36 @@
+use regex::Captures;
+use regex::Regex;
+use std::collections::HashSet;
+use std::str::FromStr;
+
+fn day_4(input: String) -> usize {
+    // This was my initial regex
+    // It's already filtering out some bad entries, leading to less matching wrt requested value.
+    //let passport_regex = Regex::new(r"(?:(?P<byr>byr:[0-9]+)\s?|(?P<iyr>iyr:[0-9]+)\s?|(?P<eyr>eyr:[0-9]+)\s?|(?P<hgt>hgt:[0-9]+(?:cm|in)\s?)|(?P<ecl>ecl:[a-z]{3})\s?|(?P<hcl>hcl:#[a-z0-9]+)\s?|(?P<pid>pid:[0-9]+\s?)|(?P<cid>cid:[0-9]+)\s?)+\n").unwrap();
+    let passport_regex = Regex::new(r"[a-z]{3}:[a-z#0-9]+\s?").unwrap();
+    Regex::new("\n\n")
+        .unwrap()
+        .split(input.as_str())
+        .map(|raw| {
+            passport_regex
+                .captures_iter(raw)
+                .filter(|capture| !capture.get(0).unwrap().as_str().contains("cid"))
+                .count()
+        })
+        .filter(|count| count == 7)
+        .count()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::day_4::day_4;
+    use crate::shared::read_all;
+
+    #[test]
+    fn test_day_4() {
+        let input = read_all("4-test");
+        assert_eq!(day_4(input), 2);
+        let input = read_all("4");
+        assert_eq!(day_4(input), 170);
+    }
+}
--- 2020/src/shared.rs
+++ 2020/src/shared.rs
@@ -1,8 +1,17 @@
 use std::fmt::Debug;
 use std::fs::File;
 use std::io;
-use std::io::BufRead;
+use std::io::{BufRead, Read};
 use std::str::FromStr;
+pub fn read_all(day: &str) -> String {
+    let input_path = format!("inputs/day-{}.txt", day);
+    let mut buffer = String::new();
+    File::open(input_path)
+        .unwrap()
+        .read_to_string(&mut buffer)
+        .unwrap();
+    buffer
+}
 
 pub fn load_input_parsed<T>(day: &str) -> Vec<T>
 where
