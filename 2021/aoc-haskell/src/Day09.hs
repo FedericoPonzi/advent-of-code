24df86c33206f65cf5de038319e2834fc928f8ed
Federico Ponzi <me@fponzi.me>
2021-12-10 14:14:01 +0000

    2021: Day 09 part 2

--- 2021/aoc-haskell/src/Day09.hs
+++ 2021/aoc-haskell/src/Day09.hs
@@ -1,25 +1,65 @@
-{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-module Day09 where
-import GHC.Generics (Generic)
-import Data.Hashable
-import Control.Monad (join)
-import Data.Graph.UGraph
-import Data.Graph.Types
 
-type Label = Int
-type Depthness = Int
+module Day09 (day09, day09p2) where
 
-data Element = Element Depthness Label
-    deriving (Show, Ord, Eq, Generic)
-instance Hashable Element
+import Data.List
+import Data.Maybe (fromMaybe)
 
+safeGet :: Int -> [a] -> Maybe a
+safeGet n arr = if n < len && n >= 0 then Just (arr !! n) else Nothing where len = length arr
+
+safeGetPos :: Int -> Int -> [[Int]] -> Maybe Int
+safeGetPos row col m = safeGet col =<< safeGet row m
+
+innerLength :: Foldable t => [t a] -> Int
+innerLength l = length (head l)
+
+isLowest :: (Foldable t, Ord a) => (a, t a) -> Bool
 isLowest (d, adj) = do
-   (length $ filter(<=d) adj) == 0
+  not (any (<= d) adj)
 
 -- Calculate risk level of lowest points
 day09 :: [(Int, [Int])] -> Int
 day09 adjList = do
-   let lowestList :: [(Int, [Int])] = filter isLowest adjList
-   let lowestRiskList :: [Int] = map (\a -> fst a +1)  lowestList
-   sum lowestRiskList
+  let lowestList :: [(Int, [Int])] = filter isLowest adjList
+  let lowestRiskList :: [Int] = map (\a -> 1 + fst a) lowestList
+  sum lowestRiskList
+
+findBasins m rowLen colLen visited row col = do
+  let index = (row, col)
+  if index `elem` visited
+    then (0, visited)
+    else
+      if (m !! row !! col) == 9
+        then (0, index : visited)
+        else do
+          let filterValid (r, c) = 0 <= r && r < rowLen && 0 <= c && c < colLen
+          let directions = filter filterValid [(row -1, col), (row + 1, col), (row, col - 1), (row, col + 1)]
+          let foldFn (accBasins, accVisited) (r, c) =
+                let (bFound, visitedb) = findBasins m rowLen colLen accVisited r c
+                 in (bFound + accBasins, visitedb)
+          let (basins, vistedd) = foldl foldFn (0, index : visited) directions
+          (basins + 1, vistedd)
+
+countBasins rowLen colLen m toVisit visited basins = do
+  let unvisited = take 1 toVisit
+  if null unvisited
+    then (basins, visited)
+    else do
+      let (row, col) = head unvisited
+      let (b, visitedNew) = findBasins m rowLen colLen [] row col
+      countBasins rowLen colLen m (toVisit \\ visitedNew) (visitedNew ++ visited) (b : basins)
+
+filterNines matrix toVisit = do
+  let isNine (r, c) = matrix !! r !! c == 9
+  filter isNine toVisit
+
+day09p2 :: [[Int]] -> Int
+day09p2 matrix = do
+  let rowLen = length matrix
+  let colLen = innerLength matrix
+  let zipped = concatMap (\a -> zip (repeat a) [0 .. colLen -1]) [0 .. rowLen -1]
+  let visited = filterNines matrix zipped
+  let totalBasins = countBasins rowLen colLen matrix zipped visited []
+  let (basins, visited) = totalBasins
+  product $ take 3 $ reverse $ sort basins
--- 2021/aoc-haskell/test/Day09_test.hs
+++ 2021/aoc-haskell/test/Day09_test.hs
@@ -3,14 +3,12 @@
 module Day09_test where
 
 import Control.Monad (join)
-import Data.Graph.UGraph
 import Data.List.Split
-import qualified Data.Map as DataMap
 import Data.Maybe (fromMaybe)
 import Day09
 import System.IO
 import Test.HUnit
-import Data.Graph.Types
+
 
 simpleInput =
   "2199943210\n\
@@ -18,15 +16,11 @@ simpleInput =
   \9856789892\n\
   \8767896789\n\
   \9899965678"
-
 safeGet :: Int -> [a] -> Maybe a
-safeGet n arr = do
-  let len = length arr
-  if n < len && n >= 0 then Just (arr !! n) else Nothing
+safeGet n arr =  if n < len && n >= 0 then Just (arr !! n) else Nothing where len = length arr
 
 safeGetPos :: Int -> Int -> [[Int]] -> Maybe Int
-safeGetPos row col m = do
-  safeGet col =<< safeGet row m
+safeGetPos row col m = safeGet col =<< safeGet row m
 
 innerLength l = length (head l)
 
@@ -36,34 +30,36 @@ adjancetBuilder r c m = do
   let index = r * innerLength m + c
   depth
 
+north :: Int -> Int -> [[Int]] -> Int
 north row = adjancetBuilder (row -1)
 
+south :: Int -> Int -> [[Int]] -> Int
 south row = adjancetBuilder (row + 1)
 
+east :: Int -> Int -> [[Int]] -> Int
 east row col = adjancetBuilder row (col - 1)
 
+weast :: Int -> Int -> [[Int]] -> Int
 weast row col = adjancetBuilder row (col + 1)
 
 
 parseInput :: String -> [(Int, [Int])]
 parseInput contents = do
   let asLines :: [[Int]] = map (map read . chunksOf 1) (lines contents)
-  let lines_length = length (head asLines)
   let zipped = zip [0 ..] $ map (zip [0 ..]) asLines
   let getEdges row col = do [north row col asLines, south row col asLines, east row col asLines, weast row col asLines]
-  let edgeBuilder edges row col depth = do
+  let edgeBuilder edges depth = do
         let ret =  [(depth, [edges !! 0, edges !! 1, edges !! 2, edges !! 3])]
         ret
 
-  concatMap (\(indexRow, rowList) -> concatMap (\(indexCol, depthVal) -> edgeBuilder (getEdges indexRow indexCol) indexRow indexCol depthVal ) rowList) zipped
+  concatMap (\(indexRow, rowList) -> concatMap (\(indexCol, depthVal) -> edgeBuilder (getEdges indexRow indexCol) depthVal ) rowList) zipped
 
 testSimple :: Test
 testSimple =
   TestCase
     ( do
         let pos = parseInput simpleInput
-        print pos
-        assertEqual "day 08 simple" 26 (day09 pos)
+        assertEqual "day 08 simple" 15 (day09 pos)
     )
 
 test1 :: Test
@@ -74,10 +70,36 @@ test1 =
         contents :: String <- hGetContents handle
         let pos = parseInput contents
         assertEqual
-          "test 2"
-          973499
+          "test 1"
+          591
           (day09 pos)
         hClose handle
     )
+
+parseInput2 :: String -> [[Int]]
+parseInput2 contents = map (map read . chunksOf 1) (lines contents)
+
+testSimple2 :: Test
+testSimple2 =
+  TestCase
+    ( do
+        let pos = parseInput2 simpleInput
+        print pos
+        assertEqual "day 08 simple2 " 1134 (day09p2 pos)
+    )
+test2 :: Test
+test2 =
+  TestCase
+    ( do
+        handle <- openFile "inputs/day09.txt" ReadMode
+        contents :: String <- hGetContents handle
+        let pos = parseInput2 contents
+        assertEqual
+          "test 2"
+          1113424
+          (day09p2 pos)
+        hClose handle
+    )
+
 tests :: [Test]
-tests = [testSimple,test1]
+tests = [test1, testSimple, testSimple2, test2]
