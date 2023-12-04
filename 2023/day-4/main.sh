87fec8e13ebe139f95e40799e3aeb01d8249883e
Federico Ponzi <me@fponzi.me>
2023-12-04 13:50:11 +0000

    2023 day 4 part 2

--- 2023/day-4/main.sh
+++ 2023/day-4/main.sh
@@ -30,7 +30,7 @@ get_points() {
     fi
 }
 
-solve() {
+solve1() {
     filename=$1
     total=0
     while read -r line; do 
@@ -40,15 +40,84 @@ solve() {
     done < $filename
     echo $total;
 }
-example=$(solve "example.txt")
+example=$(solve1 "example.txt")
 if [ $example != 13 ]; then 
     echo "Example failed: received $example, expected: 13";
     exit 1;
 fi
 
-input=$(solve "input.txt")
+input=$(solve1 "input.txt")
 if [ $input != 20107 ]; then 
     echo "part 1 failed: received $example, expected: 20107";
+    exit 1;
+fi
+
+# associative array with the card copies
+declare -A copies
+
+get_points2() {
+    line_number=$1
+    line=$2
+    line=${line#*: }
+    IFS="|" read -a cards <<<"$line"
+    IFS=" " read -a winning_numbers <<<"${cards[0]}"
+    IFS=" " read -a owned_numbers <<<"${cards[1]}"
+    #echo "Winning numbers: ${winning_numbers[*]}"
+    #echo "Owned numbers: ${owned_numbers[*]}"
+    # check how many owned numbers are in winning numbers
+    total=0
+    for i in "${owned_numbers[@]}"; do
+        for j in "${winning_numbers[@]}"; do
+            if [ "$i" = "$j" ]; then
+                    total=$((total+1))
+                break
+            fi
+        done
+    done
+    #check if total is -1, if so, then the card is not won
+    if [ $total = 0 ]; then
+        return;
+    fi
+
+    my_copies=${copies[$line_number]}
+
+    for ((i=$line_number+1; i<=$line_number+$total;i++)); do
+        if [ "${copies[$i]}" = "" ]; then
+            copies[$i]=1;
+        fi
+        copies[$i]=$(($my_copies+copies[$i]));
+        done
+}
+
+solve2() {
+    filename=$1
+    line_number=1
+    copies[1]=1
+    lines=$(wc -l $filename | awk '{print $1}')
+    for ((i=1;i<=$lines;i++)); do
+        copies[$i]=1;
+    done
+    while read -r line; do 
+        get_points2 $line_number "$line"
+        line_number=$((line_number+1))
+    done < $filename
+
+    # sum all values of copies
+    total=0
+    for i in "${!copies[@]}"; do
+        total=$((total+copies[$i]))
+    done
+    echo $total;
+}
+example=$(solve2 "example.txt")
+if [ $example != 30 ]; then
+    echo "Example failed: received $example, expected: 30";
+    exit 1;
+fi
+
+input=$(solve2 "input.txt")
+if [ $input != 30 ]; then
+    echo "Input failed: received $input, expected: 30";
     exit 1;
 fi
 
