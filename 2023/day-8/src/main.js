0b66e14205ac861e501ace674ec891e0e3716437
Federico Ponzi <me@fponzi.me>
2023-12-08 20:58:12 +0000

    2023 day7 part 2

--- 2023/day-8/src/main.js
+++ 2023/day-8/src/main.js
@@ -1,35 +1,52 @@
 const fs = require("fs");
 
+function gcd(a, b) {
+    return b === 0 ? a : gcd(b, a % b);
+}
+function findLCM(arr) {
+    let result = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        result = (result * arr[i]) / gcd(result, arr[i]);
+    }
+    return result;
+}
 
 function parse(filename) {
     let input = fs.readFileSync(filename, 'utf8').split('\n');
     let instructions = input[0].split("").map((c) => c === "L"? 0 : 1);
-    let ret = {};
-    // iterate on the rest of the input, break the line "aa = (ab, bc)"
-    // and create a new node(aa, ab, bc)
+    let ret = new Map();
     for (let i = 2; i < input.length; i++) {
         let [name, value] = input[i].split(' = ');
         let [left, right] = value.split(', ');
-        ret[name] = [left.substring(1), right.substring(0, right.length-1)]
+        ret.set(name, [left.substring(1), right.substring(0, right.length-1)])
     }
     return [instructions, ret];
 }
-const INITIAL_NODE = "AAA";
-const FINAL_NODE = "ZZZ";
 
-function visit(instructions, nodes) {
-    let next = INITIAL_NODE;
+function visit(instructions, nodes, start_node, end_node) {
+    let next = start_node;
     let index = 0;
-    while (next !== FINAL_NODE) {
-        next = nodes[next][instructions[index%instructions.length]];
+    while (!next.endsWith(end_node)) {
+        next = nodes.get(next)[instructions[index%instructions.length]];
         index = index + 1; 
     }
     return index;
 }
 
-function solve1(filename) {
+
+function solve2(filename) {
     let content = parse(filename);
-    return visit(content[0], content[1]);
+    var paths_length = [...content[1].keys()]
+        .filter(k => k.endsWith("A"))
+        .map(start_node => visit(content[0], content[1], start_node, "Z"))
+    return findLCM(paths_length);
 }
 
-module.exports = solve1;
+
+function solve1(filename) {
+    let content = parse(filename);
+    return visit(content[0], content[1], "AAA", "ZZZ");
+}
+
+
+module.exports = [solve1, solve2];
--- 2023/day-8/src/main.test.js
+++ 2023/day-8/src/main.test.js
@@ -1,7 +1,13 @@
-const solve1 = require('./main');
+const [solve1, solve2] = require('./main');
 
 test('part 1', () => {
   expect(solve1('example.txt')).toBe(2);
   expect(solve1('example2.txt')).toBe(6);
   expect(solve1('input.txt')).toBe(18827);
+});
+
+test('part 2', () => {
+  expect(solve2('example.txt')).toBe(2);
+  expect(solve2('example2.txt')).toBe(6);
+  expect(solve2('input.txt')).toBe(20220305520997);
 });
